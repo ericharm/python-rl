48fbc7fbd886ea2b39f2a57b45fb8c0689797adb
ericharm <ericharm@gmail.com>
2017-09-01 02:43:29 -0400

    write a test

---
+++ test/level_test.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import unittest
+
+sys.path.insert(0,os.path.abspath(__file__+"/../.."))
+
+from lib.tile import Tile
+
+class TileTest(unittest.TestCase):
+
+  def setUp(self):
+    self.tile = Tile(2,3)
+
+  def test_init(self):
+    self.assertEqual(2, self.tile.location['x'])
+
+  def test_set_type(self):
+    self.tile.set_type("floor")
+    self.assertEqual("floor", self.tile.type)
+
+  def test_char(self):
+    self.tile.type = "wall"
+    self.assertEqual("=", self.tile.char())
+
+  def test_color(self):
+    self.assertEqual("BLACK", self.tile.color())
+
+if __name__ == '__main__':
+  unittest.main()
+
