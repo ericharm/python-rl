e123808d1338dd67051904237b9491e575bf0908
ericharm <ericharm@gmail.com>
2017-08-31 23:34:43 -0400

    game class

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 from curses import *
 import yaml
-from lib.level import Level
+from lib.game import Game
 
 with open("config/config.yml", 'r') as stream:
     try:
@@ -9,45 +9,9 @@ with open("config/config.yml", 'r') as stream:
         print(exc)
 
 
-LEVEL_WIDTH = config['level']['width']
-LEVEL_HEIGHT = config['level']['height']
-
-level = Level(LEVEL_WIDTH,LEVEL_HEIGHT)
-
-def add_floor_tile(char):
-    tile = level.tiles[char['x']][char['y']]
-    tile.set_type("floor")
-
 def main(stdscr):
-
-    curs_set(0)
-    init_pair(1, COLOR_MAGENTA, COLOR_BLACK)
-
-    char = {"x": 4, "y": 4}
-    target = level.tiles[20][12]
-
-    key_in = ""
-
-    while (key_in != "q"):
-      # draw
-      stdscr.clear()
-      level.draw(stdscr)
-      stdscr.addstr(char['y'], char['x'], '@', color_pair(1))
-
-      # get input
-      key_in = stdscr.getkey()
-      if (key_in == "KEY_LEFT" and char['x'] > 0):
-          char['x'] -= 1
-      elif (key_in == "KEY_RIGHT" and char['x'] < LEVEL_WIDTH - 1):
-          char['x'] += 1
-      elif (key_in == "KEY_UP" and char['y'] > 0):
-          char['y'] -= 1
-      elif (key_in == "KEY_DOWN" and char['y'] < LEVEL_HEIGHT - 1):
-          char['y'] += 1
-
-      # update
-      add_floor_tile(char)
-
+    game = Game(config)
+    game.run(stdscr)
 
 wrapper(main)
 
---
+++ lib/game.py
@@ -0,0 +1,45 @@
+from level import Level
+from curses import *
+
+class Game:
+
+    def __init__(self, config):
+        self.LEVEL_WIDTH = config['level']['width']
+        self.LEVEL_HEIGHT = config['level']['height']
+        self.level = Level(self.LEVEL_WIDTH, self.LEVEL_HEIGHT)
+
+    def set_tile_type_at_char(self, char, type):
+        tile = self.level.tiles[char['x']][char['y']]
+        tile.set_type(type)
+
+    def run(self, stdscr):
+
+        curs_set(0)
+        init_pair(1, COLOR_MAGENTA, COLOR_BLACK)
+
+        char = {"x": 4, "y": 4}
+        target = self.level.tiles[20][12]
+
+        key_in = ""
+
+        while (key_in != "q"):
+          # draw
+          stdscr.clear()
+          self.level.draw(stdscr)
+          stdscr.addstr(char['y'], char['x'], '@', color_pair(1))
+
+          # get input
+          key_in = stdscr.getkey()
+          if (key_in == "KEY_LEFT" and char['x'] > 0):
+              char['x'] -= 1
+          elif (key_in == "KEY_RIGHT" and char['x'] < self.LEVEL_WIDTH - 1):
+              char['x'] += 1
+          elif (key_in == "KEY_UP" and char['y'] > 0):
+              char['y'] -= 1
+          elif (key_in == "KEY_DOWN" and char['y'] < self.LEVEL_HEIGHT - 1):
+              char['y'] += 1
+
+          # update
+          self.set_tile_type_at_char(char, "floor")
+
+
