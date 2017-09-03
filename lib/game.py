7702e8cb520736e60f7c18c1b921273acc311ea9
ericharm <ericharm@gmail.com>
2017-09-03 04:32:27 -0400

    hero class

--- lib/game.py
+++ lib/game.py
@@ -1,4 +1,5 @@
 from level import Level
+from hero import Hero
 from curses import *
 import random
 
@@ -8,35 +9,37 @@ class Game:
     self.config = config
     self.level = Level(config['level'])
     self.level.generate(config['level'])
-
-  def is_floor(self, x, y):
-        return self.level.tiles[x][y].type == "floor"
+    hero_start = self.config['hero']['start']
+    self.hero = Hero(hero_start['x'], hero_start['y'])
 
   def run(self, screen):
 
-    curs_set(0)
-    init_pair(1, COLOR_MAGENTA, COLOR_BLACK)
-
-    char = self.config['hero']['start']
-
+    self.init_curses()
     key_in = ""
-
     while (key_in != "q"):
       # draw
       screen.clear()
       self.level.draw(screen)
-      screen.addstr(char['y'], char['x'], '@', color_pair(1))
+      screen.addstr(self.hero.y, self.hero.x, '@', color_pair(1))
 
       # get input
       key_in = screen.getkey()
-      x = char['x']
-      y = char['y']
+      x = self.hero.x
+      y = self.hero.y
       if (key_in == "KEY_LEFT" and self.is_floor(x - 1, y)):
-        char['x'] -= 1
+        self.hero.x -= 1
       elif (key_in == "KEY_RIGHT" and self.is_floor(x + 1, y)):
-        char['x'] += 1
+        self.hero.x += 1
       elif (key_in == "KEY_UP" and self.is_floor(x, y - 1)):
-        char['y'] -= 1
+        self.hero.y -= 1
       elif (key_in == "KEY_DOWN" and self.is_floor(x, y + 1)):
-        char['y'] += 1
+        self.hero.y += 1
+
+  def init_curses(self):
+    curs_set(0)
+    init_pair(1, COLOR_MAGENTA, COLOR_BLACK)
+
+  def is_floor(self, x, y):
+        return self.level.tiles[x][y].type == "floor"
+
 
---
+++ lib/hero.py
@@ -0,0 +1,6 @@
+class Hero:
+
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
