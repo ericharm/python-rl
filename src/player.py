6821eb8fe52f0d2cf9b1a3c95be2c4adcbed90e1
ericharm <ericharm@gmail.com>
2017-10-01 16:13:50 -0400

    tiles are responsible for drawing their entities

--- src/player.py
+++ src/player.py
@@ -9,6 +9,7 @@ class Player:
     x = hero.x
     y = hero.y
     if   (key == "KEY_LEFT" and self.game.is_walkable(x - 1, y)):
+      # put this in a function entity.move(x,y)
       tiles[x][y].entities.remove(hero)
       tiles[x-1][y].entities.append(hero)
       hero.x -= 1
@@ -23,7 +24,7 @@ class Player:
     elif (key == "KEY_DOWN" and self.game.is_walkable(x, y + 1)):
       tiles[x][y].entities.remove(hero)
       tiles[x][y+1].entities.append(hero)
-      hero.y += 1 
+      hero.y += 1
     elif (key is ">" and self.game.level.tiles[x][y].type is "stairs_down"):
       self.game.descend_stairs()
     elif (key is "<" and self.game.level.tiles[x][y].type is "stairs_up"):
